8fc55ee654cda9f202847ebb33557d2447e30d53
Tea <tealalala@protonmail.com>
2019-02-22 15:31:54 -0800

    refactor javascript code in main.js

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 // Table of Contents
 // 01 - Array of Quotes
 
-
 var quotes = [
   ["Successful organizing forces you to look at the big picture, not one small section of the frame, so that the system you design will be complete.", "Julie Morgenstern", "Author, Organization and Time Management Consultant"],
   ["Organizing is what you do before you do something, so that when you do it, it is not all mixed up.", "A.A. Milne", "author known for \"Winnie-the-Pooh\""],
   ["For every minute spent in organizing, an hour is earned.", "Benjamin Franklin", "One of the Founding Fathers of the United States"]
 ];
 
-var randomNumber = Math.floor(Math.random() * quotes.length);
-console.log(randomNumber);
-
-document.getElementById('text').innerHTML = quotes[randomNumber][0];
-
-document.getElementById('new-quote').addEventListener("click", function(){
+function randomQuote() {
   var randomNumber = Math.floor(Math.random() * quotes.length);
-  document.getElementById('text').innerHTML = quotes[randomNumber][0];
 
   console.log(randomNumber);
   console.log("successful click");
-});
+
+  return document.getElementById('text').innerHTML = quotes[randomNumber][0];
+};
+
+document.getElementById('new-quote').addEventListener("click", randomQuote, true);
