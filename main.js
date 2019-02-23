b7440030793f85c27372c2c16b7e4d336c741fdb
Tea <tealalala@protonmail.com>
2019-02-22 16:04:47 -0800

    add basic css

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 // Table of Contents
 // 01 - Array of Quotes
+// 02 - getRandomQuote function
+// 03 - AddEventListener - new quote when click button
 
+// 01 - Array of Quotes
 var quotes = [
   ["Successful organizing forces you to look at the big picture, not one small section of the frame, so that the system you design will be complete.", "Julie Morgenstern", "Author, Organization and Time Management Consultant"],
   ["Organizing is what you do before you do something, so that when you do it, it is not all mixed up.", "A.A. Milne", "author known for \"Winnie-the-Pooh\""],
   ["For every minute spent in organizing, an hour is earned.", "Benjamin Franklin", "One of the Founding Fathers of the United States"]
 ];
 
-function randomQuote() {
+// 02 - getRandomQuote function
+function getRandomQuote() {
   var randomNumber = Math.floor(Math.random() * quotes.length);
 
   console.log(randomNumber);
@@ -16,4 +20,5 @@ function randomQuote() {
   return document.getElementById('text').innerHTML = quotes[randomNumber][0];
 };
 
-document.getElementById('new-quote').addEventListener("click", randomQuote, true);
+// 03 - AddEventListener - new quote when click button
+document.getElementById('new-quote').addEventListener("click", getRandomQuote, true);
